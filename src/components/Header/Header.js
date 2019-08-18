96d9af47b310661f9557df2ac8146c585dc0fe45
Leonardo Cano <leocano4242@gmail.com>
2019-08-18 16:46:27 -0300

    Add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../generic/Container'
+import Section from '../generic/Section'
+
+const Header = () => {
+  return (
+    <Section>
+      <Container>
+        <h1>'Sup world</h1>
+      </Container>
+    </Section>
+  );
+}
+
+export default Header;
