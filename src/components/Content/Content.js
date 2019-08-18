1e3e69516b2511540e3d332e53ceb2543d14e5d4
Leonardo Cano <leocano4242@gmail.com>
2019-08-18 16:48:51 -0300

    Add Content component

---
+++ src/components/Content/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../generic/Container'
+import Section from '../generic/Section'
+
+const Content = () => {
+  return (
+    <Section>
+      <Container>
+        <h1>Content will go here</h1>
+      </Container>
+    </Section>
+  );
+}
+
+export default Content;
